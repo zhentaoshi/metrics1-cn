89f924a94a5587a20fc6edfb1eaea7478f5aca79
Zhentao Shi <shizhentao@gmail.com>
2022-08-23 13:39:39 +0800

    add results from Table 4. to polish

--- Replicate.R
+++ Replicate.R
@@ -1,9 +1,9 @@
 
-prefLevelTest <- read.csv(file = "prefLevelTest.csv", header = TRUE)
+d0 <- read.csv(file = "prefLevelTest.csv", header = TRUE)
 
 
 ## regression 1
-reg.ols <-lm(lngdppc2000~lnmig1127+south,data=prefLevelTest)
+reg.ols <-lm(lngdppc2000~lnmig1127+south,data=d0)
 summary(reg.ols)
 
 robust.csv <- sandwich::vcovHC(reg.ols,type="HC1")
@@ -13,8 +13,39 @@ print( lmtest::coeftest(reg.ols, vcov. = robust.csv) ) # robust test
 
 ## regression 2
 
-data=fastDummies::dummy_cols(prefLevelTest,select_columns = c("provgb"),remove_first_dummy = TRUE)
+d1 <- d0
+d1$provgb <- as.factor(d0$provgb)
 
-## Fit the regression model(column 2)
-reg.dummy<-lm(lngdppc2000~lnmig1127+south+lnhhden1080+lnarea+provgb_14+provgb_32+provgb_33+provgb_34+provgb_35+provgb_36+provgb_37+provgb_41+provgb_42+provgb_43+provgb_44+provgb_45+provgb_50+provgb_61+provgb_62,data=data)
+# data=fastDummies::dummy_cols(prefLevelTest,select_columns = c("provgb"),remove_first_dummy = TRUE)
+
+## Fit the regression model(Table II Column 2)
+reg.dummy<-lm(lngdppc2000~lnmig1127+south+lnhhden1080+lnarea+provgb,data=d1)
 summary(reg.dummy)
+
+
+## regression 3
+## Fit the regression model(Table IV Column 1)
+## Stage 1
+
+first<-lm(lnmig1127~difmigperiod+shock+south+x_coord+y_coord+coast+dist_ports+dsongrivers+lnarea+lnhhden1080+
+            nsden+provgb+lnrice+lnwheat+lnmaize+lnspotato+dist_HZ+change+n_change,data=d1)
+summary(first)
+m.hat<-fitted.values(first)
+
+
+second<-ivreg::ivreg(lngdppc2000~lnmig1127+shock+south+x_coord+y_coord+coast+dist_ports+dsongrivers
+              +lnarea+lnhhden1080+nsden+provgb+lnrice+lnwheat+lnmaize+lnspotato+dist_HZ
+              +change+n_change|
+                difmigperiod+shock+south+x_coord+y_coord+coast+dist_ports+dsongrivers+lnarea+lnhhden1080
+              +lnhhden1080+nsden+provgb+lnrice+lnwheat+lnmaize+lnspotato+dist_HZ+change+n_change,data=d1)
+summary(second)
+
+
+tsls<-lm(lngdppc2000~m.hat+shock+south+x_coord+y_coord+coast+dist_ports+dsongrivers
+         +lnarea+lnhhden1080+nsden+provgb+lnrice+lnwheat+lnmaize+lnspotato+dist_HZ
+         +change+n_change,data=d1)
+summary(tsls)
+
+
+
+
